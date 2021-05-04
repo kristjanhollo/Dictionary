b0470497e1422d572672bd4080253f21c72646a4
Kristjan <hollo.kristjan@gmail.com>
2021-05-04 23:32:01 +0300

    Removed double repeating methods, renamed methods, make it more Lego

--- src/main/java/userinterface/UserInterface.java
+++ src/main/java/userinterface/UserInterface.java
@@ -1,29 +1,26 @@
 package userinterface;
 
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
-public class UserInterface {
+public class UserInterface implements GUI {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void startProgram() {
+    public void startProgram() {
         drawBook();
-        boolean run = true;
-        while(run) {
-
-            menu();
-
+        boolean isRunning = true;
+        while (isRunning) {
+            MainMenu.show();
             switch (scanner.nextLine().toUpperCase()) {
                 case "1":
-                    makeDictionary();
+                    loadDictionary();
                     break;// to - do
                 case "2":
-                    LoadFile.menu();
+                    // TODO: change this
+                    DictionaryOperator.menu();
                     break;
                 case "Q":
-                    run = false;
+                    isRunning = false;
                     break;
                 default:
                     System.out.println("Wrong input, please choose from menu: ");
@@ -33,45 +30,24 @@ public class UserInterface {
 
     }
 
-    static void menu() {
-        System.out.println("1 - make a new dictionary");
-        System.out.println("2 - load an existing dictionary");
-        System.out.println("Q - to quit program");
-        System.out.println("――――――――――――――――――――――");
+    public void drawBook() {
+//        System.out.println("
+//                       __...--~~~~~-._   _.-~~~~~--...__
+//                    //               `V'               \\\\\s
+//                   //                 |                 \\\\\s
+//                  //__...--~~~~~~-._  |  _.-~~~~~~--...__\\\\\s
+//                 //__.....----~~~~._\\ | /_.~~~~----.....__\\\\
+//                ====================\\\\|//====================
+//                               \s");
+//
+//        System.out.println("Welcome to dictionary!\n");
     }
 
-    static void drawBook() {
-        System.out.println("""
-                       __...--~~~~~-._   _.-~~~~~--...__
-                    //               `V'               \\\\\s
-                   //                 |                 \\\\\s
-                  //__...--~~~~~~-._  |  _.-~~~~~~--...__\\\\\s
-                 //__.....----~~~~._\\ | /_.~~~~----.....__\\\\
-                ====================\\\\|//====================
-                               \s""");
-
-        System.out.println("Welcome to dictionary!\n");
-    }
-
-    private static void makeDictionary() {
-
+    private void loadDictionary() {
+        //TODO: change this
         System.out.print("Name of new dictionary: ");
         String fileName = scanner.nextLine();
-        File tempFile = new File("DictionariesCollection\\" + fileName + ".txt");
-        if(tempFile.exists()) {
-            System.out.println("Sorry, " + fileName.toUpperCase() + " already exists.");
-        } else {
-            try {
-                File myObj = new File("DictionariesCollection\\" + fileName + ".txt");
-                if (myObj.createNewFile()) {
-                    System.out.println(fileName.toUpperCase() + " has been created!");
-                    System.out.println();
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-        }
-    }
+        FileLoader.loadFile(fileName);
     }
 }
 
