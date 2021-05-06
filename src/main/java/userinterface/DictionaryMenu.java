b68973c295d8034073d4aeeeef6a078fb0e9b488
Kristjan Hollo <hollo.kristjan@gmail.com>
2021-05-06 10:20:24 +0300

    Clean up

--- src/main/java/userinterface/DictionaryMenu.java
+++ src/main/java/userinterface/DictionaryMenu.java
@@ -18,7 +18,7 @@ public class DictionaryMenu {
         System.out.println("3 - to add a new word");
         System.out.println("4 - to remove a word from dictionary");
         System.out.println("5 - to go back");
-        System.out.println("Q - to quit");
+        System.out.println("Q - to quit program");
         System.out.println("――――――――――――――――――――――");
     }
 
