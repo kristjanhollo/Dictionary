feda75567a1521da9f339abaa90bb3604034bce3
Kristjan Hollo <hollo.kristjan@gmail.com>
2021-05-06 10:19:36 +0300

    Clean up

--- src/main/java/userinterface/DictionaryMenu.java
+++ src/main/java/userinterface/DictionaryMenu.java
@@ -1,16 +1,6 @@
 package userinterface;
 
 public class DictionaryMenu {
-    public static void show() {
-        System.out.println("――――――――――――――――――――――");
-        System.out.println("1 - to list all translations");
-        System.out.println("2 - to search for a translation");
-        System.out.println("3 - to add a new word");
-        System.out.println("4 - to remove a word from dictionary");
-        System.out.println("5 - to go back");
-        System.out.println("Q - to quit");
-        System.out.println("――――――――――――――――――――――");
-    }
 
     public static void showDictionaryOperationsMenu() {
         System.out.println("――――――――――――――――――――――");
