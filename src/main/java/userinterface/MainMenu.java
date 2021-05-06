925fb31690b16981b0606af346bdf6ee5e7dc741
Kristjan Hollo <hollo.kristjan@gmail.com>
2021-05-06 10:20:04 +0300

    Clean up

--- src/main/java/userinterface/MainMenu.java
+++ src/main/java/userinterface/MainMenu.java
@@ -2,6 +2,7 @@ package userinterface;
 
 public class MainMenu {
     public static void show() {
+        System.out.println("――――――――――――――――――――――");
         System.out.println("1 - make a new dictionary");
         System.out.println("2 - load an existing dictionary");
         System.out.println("Q - to quit program");
